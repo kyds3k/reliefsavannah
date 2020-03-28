27e998dd2c6c08d7ccd818c69e01f720c5065d3a
Adam Darby <adam.darby@fortyfour.com>
2020-03-27 22:56:59 -0400

    Added new bars per Chelsea

--- js/main.js
+++ js/main.js
@@ -1,34 +1,43 @@
-$('#bars').flexdatalist({
-    searchContain: true
+$("#bars").flexdatalist({
+  searchContain: true,
+  searchByWord: true
 });
 
-const inputField = document.querySelector('input[type="text"]')
+const inputField = document.querySelector('input[type="text"]');
 
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault()
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
   let searchTerm = removePunctuation(inputField.value).toLowerCase();
-    console.log(searchTerm);
-  $('.bar').stop(true, true).fadeOut();
+  console.log(searchTerm);
+  $(".bar")
+    .stop(true, true)
+    .fadeOut();
 
-
-  if (($('.bar[data-name=' + searchTerm + ']')).length) {
-    $('.bar[data-name=' + searchTerm + ']').stop(true, true).delay(500).fadeIn();
+  if ($(".bar[data-name=" + searchTerm + "]").length) {
+    $(".bar[data-name=" + searchTerm + "]")
+      .stop(true, true)
+      .delay(500)
+      .fadeIn();
   } else {
-    $('.notfound').stop(true, true).delay(500).fadeIn();
+    $(".notfound")
+      .stop(true, true)
+      .delay(500)
+      .fadeIn();
   }
-
-})
-
-$('.show-all').click(function(){
-    $('.bar').not('.notfound').fadeIn();
-    return false;
 });
 
-let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+$(".show-all").click(function() {
+  $(".bar")
+    .not(".notfound")
+    .fadeIn();
+  return false;
+});
 
-let regex = new RegExp('[' + punctuation + ']', 'g');
+let punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+
+let regex = new RegExp("[" + punctuation + "]", "g");
 
 function removePunctuation(string) {
-  string = string.replace(/\s+/g,'');
-  return string.replace(regex, '');
+  string = string.replace(/\s+/g, "");
+  return string.replace(regex, "");
 }
